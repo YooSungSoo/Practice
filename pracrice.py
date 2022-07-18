18bb9e34ff78971f0f5a731d9d83f238d464af73
SungSoo Yoo <elcane2@ajou.ac.kr>
2022-07-18 14:45:06 +0900

    Class and overriding

--- pracrice.py
+++ pracrice.py
@@ -1,6 +1,6 @@
 class FourCal:
 
-    def __init__(self,first,second):
+    def __init__(self,first,second): #생성자
         self.first = first
         self.second = second
 
@@ -24,10 +24,25 @@ class FourCal:
         result = self.first / self.second
         return result
 
-a=FourCal()
-b=FourCal()
-a.setdata(4,2)
-b.setdata(6,3)
+class MoreFourCal(FourCal): #FourCal Class 상속
 
+    def pow(self):
+        result = self.first ** self.second
+        return result
+
+class SafeFourCal(FourCal): #FourCal Class 상속
+
+    def div(self):  #오버라이딩 / 덮어쓰기 / 덮어쓴 메서드가 호출됨
+        if self.second == 0:    
+            return 0
+        else : 
+            return self.first / self.second
+
+a=FourCal(4,2)
+b=FourCal(6,3)
 print(a.add(),a.sub(),a.mul(),a.div())
 print(b.add(),b.sub(),b.mul(),b.div())
+c=MoreFourCal(10,5)
+print(c.pow())
+d=SafeFourCal(3,0)
+print(d.div())
